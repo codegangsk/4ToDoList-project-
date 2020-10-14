c5f5276679dbcdde1fdea3046f766e8c9ffe1c8d
codegangsk <kjhj0604@gmail.com>
2020-10-14 13:38:12 +0900

    define model

---
+++ 4ToDoList-project-/ToDo.swift
@@ -0,0 +1,15 @@
+//
+//  ToDo.swift
+//  4ToDoList-project-
+//
+//  Created by Sophie Kim on 2020/10/14.
+//
+
+import UIKit
+
+struct ToDo {
+    var title: String
+    var isComplete: Bool
+    var dueDate: Date
+    var notes: String?
+}
